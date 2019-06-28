9406f3d4546e7231152b2899fce226abb59bbba6
bcauchon <bcauchon@gmail.com>
2019-06-28 14:13:35 -0700

    fixed second argument in use of toBeCloseTo()

--- spec/age-converter-spec.js
+++ spec/age-converter-spec.js
@@ -1,6 +1,6 @@
 import { AgeConverter } from './../src/AgeConverter.js';
 
-const eps = 0.001;
+const precision = 2; // number of decimal points to check when comparing floating point numbers
 const ageConverter = new AgeConverter(38);
 
 describe('AgeConverter', function() {
@@ -11,24 +11,25 @@ describe('AgeConverter', function() {
 
 describe('getAge("mercury")', function() {
   it('should return earth age in Mercury years', function() {
-    expect(ageConverter.getAge("mercury")).toBeCloseTo(158.33, eps);
+    console.log(ageConverter.getAge("mercury"));
+    expect(ageConverter.getAge("mercury")).toBeCloseTo(158.33, precision);
   });
 });
 
 describe('getAge("venus")', function() {
   it('should return earth age in Venus years', function() {
-    expect(ageConverter.getAge("venus")).toBeCloseTo(61.29, eps);
+    expect(ageConverter.getAge("venus")).toBeCloseTo(61.29, precision);
   });
 });
 
 describe('getAge("mars")', function() {
   it('should return earth age in Mars years', function() {
-    expect(ageConverter.getAge("mars")).toBeCloseTo(20.21, eps);
+    expect(ageConverter.getAge("mars")).toBeCloseTo(20.21, precision);
   });
 });
 
 describe('getAge("Jupiter")', function() {
   it('should return earth age in Jupiter years', function() {
-    expect(ageConverter.getAge("jupiter")).toBeCloseTo(3.20, eps);
+    expect(ageConverter.getAge("jupiter")).toBeCloseTo(3.20, precision);
   });
 });
