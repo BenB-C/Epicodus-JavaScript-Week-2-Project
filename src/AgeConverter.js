343ab43ee81bc67ac1e415347dda29baf13f7e15
bcauchon <bcauchon@gmail.com>
2019-06-28 13:28:41 -0700

    refactor AgeConverter

--- src/AgeConverter.js
+++ src/AgeConverter.js
@@ -1,15 +1,15 @@
 export class AgeConverter {
   constructor (earthAge) {
     this.earthAge = earthAge;
-    this.conversionFactors = {
-      mercury: 1 / 0.24,
-      venus: 1 / 0.62,
-      mars: 1 / 1.88,
-      jupiter: 1 / 11.86,
+    this.conversionQuotients= {
+      mercury:  0.24,
+      venus:    0.62,
+      mars:     1.88,
+      jupiter: 11.86,
     };
   }
 
   getAge(planet) {
-    return this.earthAge * this.conversionFactors[planet];
+    return this.earthAge / this.conversionQuotients[planet];
   }
 }
