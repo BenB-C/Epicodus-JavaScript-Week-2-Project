521695c9c939d0e0bb2fe016a78d06e216ba2a54
bcauchon <bcauchon@gmail.com>
2019-06-30 16:26:52 -0700

    Add script code for ui

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import AgeConverter from './AgeConverter.js';
+import LifeExpectancyCalculator from './LifeExpectancyCalculator.js';
 
 $(document).ready(function (){
-  $("body").append(`<p>document ready</p>`);
+  $("form#age-calculator").submit((event) => {
+    event.preventDefault();
 
+    let age = parseInt($("#age-input").val());
+    if (!age) {
+      $("#age-output").text("Please enter your age");
+      $("#life-output").text("");
+      return;
+    }
+    const planet = $("#planet-select").val().toLowerCase();
+
+    const ageConverter = new AgeConverter(age);
+    $("#age-output").text(ageConverter.getAgeDescription(planet));
+
+    const lifeExpectancyCalculator = new LifeExpectancyCalculator();
+    const lifeExpectancy = lifeExpectancyCalculator.getLifeExpectancy(age);
+    const planetLifeExpectancy = (new AgeConverter(lifeExpectancy)).getAge(planet);
+    $("#life-output").text(lifeExpectancyCalculator.getLifeExpectancyDescription(planetLifeExpectancy));
+  });
 });
